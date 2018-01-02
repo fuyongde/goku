c53a65c9918cc198e6470b8429998b4aae3d98e2
fuyongde <fuyongde@foxmail.com>
2018-01-03 00:03:12 +0800

    采用简单的定时任务

---
+++ dubbo-demo/dubbo-provider/src/main/java/com/goku/dubbo/provider/job/SimpleJob.java
@@ -0,0 +1,22 @@
+package com.goku.dubbo.provider.job;
+
+import com.goku.dubbo.commons.consts.DatePattern;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class SimpleJob {
+
+  @Scheduled(initialDelay = 1000L, fixedRate = 5000L)
+  public void reportCurrentTime() {
+    System.out.println("现在时间：" + DateFormatUtils.format(new Date(), DatePattern.PATTERN_1));
+  }
+
+  @Scheduled(cron="*/5 * * * * *")
+  public void sendMail() {
+    System.out.println("发送邮件");
+  }
+}
